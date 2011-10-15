00c6324342bc9297a0c97105fb087e0698b0be3f
Diego Augusto <diego.fariabarros@gmail.com>
2011-10-15 18:28:45 -0300

    Implementacao da api do google maps no cadastro de cidades e unificacao do arquivo CSS para o arquivo estilo.css localizado na pasta Estilos

--- Scripts/mapa.js
+++ Scripts/mapa.js
@@ -23,7 +23,8 @@ function inicializa () {
 } // Fim da função inicializa
 
 function codificaEndereco(){
-	var endereco = document.getElementById("endereco").value;
+	
+	var endereco = pegaEndereco();
 	
 	if (geocodificador) {
   	geocodificador.geocode( {'address':endereco}, function (resultados, status) {
@@ -43,6 +44,54 @@ function codificaEndereco(){
   		}
   	});
   }
+}
+
+
+function pegaEndereco (argument) {
+	
+	var endereco = document.getElementById("endereco").value;
+	
+	
+		
+		
+	return endereco;
+  
+}
+
+function codificaNomeCidade () {
+
+    var endereco = endereco = document.getElementById("nome").value;
+    geocodificador = new google.maps.Geocoder();
+	
+	if (geocodificador) {
+  	geocodificador.geocode( {'address':endereco}, function (resultados, status) {
+
+  		if(status == google.maps.GeocoderStatus.OK){
+
+  		   var coordenadas = resultados[0].geometry.location;
+  		   
+  		   atualizaCoordenadas(coordenadas);
+
+			var opcoes = {
+				zoom: 25,
+				center: resultados[0].geometry.location,
+				mapTypeId:google.maps.MapTypeId.ROADMAP
+			};
+	
+    		mapa = new google.maps.Map(document.getElementById('container-mapa'), opcoes);
+  			marcador = new google.maps.Marker({ 
+  				map:mapa,
+  			    draggable: true,
+  				position: resultados[0].geometry.location
+  			});
+  		}
+  		else
+  		{
+  			alert("O geocodificador não completou a requisição devidao o seguinte motivo  " + status);
+  		}
+  	});
+  }
+  	
 }
 
 
@@ -79,6 +128,19 @@ function pegaCoordenadas(){
 	return new google.maps.LatLng(latitude, longitude);
 }
 
+
+function atualizaCoordenadas (coordenadas) {
+	
+	
+	latlng = coordenadas.toString().split(",", 2);
+  		   
+  
+  		   document.getElementById('latitude').value = latlng[0].substring(1);
+  		   document.getElementById('longitude').value = latlng[1].substring(0, latlng[1].length - 1);
+  		   
+	
+  
+}
 
 
 function atualizaCampos (resultados) {
