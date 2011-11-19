34dd94e6bf03ac803b42ac373a1fc3a3df18f24c
Diego Augusto <diego.fariabarros@gmail.com>
2011-11-19 17:44:04 -0200

    Criacao do Projeto em Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HospedagemParaACopa::Application.config.secret_token = '42ffa0c7a196c04e16a40edcff5dbc8dcbee087abdd7a145c10c107899b6b6218987892d650224c0bbe2b99b333b60bbef1775bb795fed9e8a9b3eb6aefade73'
