a14258543e7cd4e2a0b9e4517f219ebe865a4165
Diego Augusto <diego.fariabarros@gmail.com>
2011-11-19 23:24:14 -0200

    Implementacao pagina inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HospedagemParaACopa::Application.routes.draw do
+  get "home/index"
+
   get "inicio/Index"
 
   devise_for :users
@@ -52,7 +54,7 @@ HospedagemParaACopa::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-    root :to => 'inicio#Index'
+    root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
